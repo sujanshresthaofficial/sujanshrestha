3b447e87b6242ffb17769f009c2832a8c7219112
Sujan Shrestha <itssujanshrestha@gmail.com>
2025-12-04 00:50:10 +0545

    Add files via upload
    first commit

---
+++ javascript.js
@@ -0,0 +1,21 @@
+const toggleSwitch = document.getElementById('checkbox');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.body.classList.add(currentTheme);
+    if (currentTheme === 'dark-mode') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light-mode');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
